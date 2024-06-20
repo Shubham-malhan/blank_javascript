a2dec4047d98b3816986e8a0de78bdf16d2ba9cc
Shubham Malhan <shubhammalhan00@gmail.com>
2024-06-20 11:09:10 +0000

    looping in array

---
+++ practice/looping in array.js
@@ -0,0 +1,10 @@
+let heroes = ["Batman", "Ironman", "Spiderman", "Shaktiman", "hulk"];
+// simple for loop
+for (let i = 0; i < heroes.length; i++){
+    console.log(heroes[i]);
+} 
+console.log("\n")
+// for of loop
+for (let hero of heroes){
+    console.log(hero);
+}
