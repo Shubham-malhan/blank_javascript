7b6703c3a900bc0103554f93562a69466d511289
Shubham Malhan <shubhammalhan00@gmail.com>
2024-07-02 12:37:09 +0000

    about operaters

--- 01_basics/_conversionoperation.js
+++ 01_basics/_conversionoperation.js
@@ -1,11 +1,11 @@
 let score = "33abc";
  
-console.log(typeof score);
-console.log(typeof (score));
+// console.log(typeof score);
+// console.log(typeof (score));
 
 let valueInNumber = Number(score);
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
 
 // "33" => 33
 // "33abc" => NaN
@@ -14,6 +14,28 @@ console.log(valueInNumber);
 
 let isLoggedIn = 1;
 
-let booleanIsLoggedIn = Boolean(isLoggedIn);
-console.log( booleanIsLoggedIn);
+// let booleanIsLoggedIn = Boolean(isLoggedIn);
+// console.log( booleanIsLoggedIn);
+
+// ******************** Operations ************************
+
+let value = 3;
+let negvalue = -value;
+
+// console.log(negvalue);
+// console.log(3+3);
+// console.log(3-3);
+// console.log(3*3);
+// console.log(3**3);
+// console.log(3/3);
+// console.log(3%3);
+
+let str1 = "hello";
+let str2 = " Shubham"; 
+str1 = str1.toUpperCase();
+str2 = str2.toUpperCase();
+
+let str3 = str1 + str2 ;
+
+console.log(str3);
  
