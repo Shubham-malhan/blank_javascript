f7805c871b7249e4631c9dbe393833b52a35f234
Shubham Malhan <shubhammalhan00@gmail.com>
2024-07-05 08:52:53 +0000

    update in this

---
+++ 01_basics/test.js
@@ -0,0 +1,17 @@
+function equal(str1,str2){
+    return str1.toUpperCase()  == str2.toUpperCase();
+    console.log(str1);
+    console.log(str2);
+    console.log(str1.toUpperCase()  === str2.toUpperCase());
+}
+
+// function equal(num1,num2){
+//     num1 === num2;
+//     console
+// }
+
+let a = 'Sarwan';
+let b = 'sarwan';
+
+let check  = equal(a,b);
+console.log(equal(a,b));
