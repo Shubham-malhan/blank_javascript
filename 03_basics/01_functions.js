bf0ee919e369c4a97508a5bd2c85804b68995c22
Shubham Malhan <shubhammalhan00@gmail.com>
2024-07-12 04:46:54 +0000

    function in js

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -31,4 +31,34 @@ function loginUserMessage(username = "User"){
     return `${username} just Logged in`;
 }
 
-console.log(loginUserMessage()); 
+// console.log(loginUserMessage()); 
+// console.log(loginUserMessage("Shubham"));
+
+function calculateCartPrice(...num1){ // Rest operator (...) 
+    return num1
+}
+
+// console.log(calculateCartPrice(200, 400, 500));
+
+const user = {
+    username: "hitesh",
+    price: 199
+}
+
+function handleObject(anyobject){
+     console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);
+}
+
+handleObject(user);
+handleObject({
+    username: "shubham",
+    price: 399
+})
+
+const myNewArray = [200, 400, 100, 600]
+ 
+function returnSecondValue(getArray){ 
+    return getArray[1];
+}
+
+console.log(returnSecondValue(myNewArray));
