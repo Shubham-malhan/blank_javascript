cc30f9618e47efae828bb07156cf9933ec739caf
Shubham Malhan <shubhammalhan00@gmail.com>
2024-07-13 17:23:33 +0000

    day1 of my course

---
+++ chaicode/day1.js
@@ -0,0 +1,43 @@
+// Variables Declaration
+
+var number = 123;
+console.log(number);
+console.log(typeof number);
+
+var string = "shubham";
+console.log(string);
+console.log(typeof string);
+
+const bool = false ;
+console.log(bool);
+console.log(typeof bool);
+
+let obj = {
+    name: "shubham",
+    rollno: 123,
+    year: 2
+};
+console.log(obj);
+console.log(typeof obj);
+
+let arr = ["hero", "zero", "power"];
+console.log(arr);
+console.log(typeof arr);
+
+let nu = null;
+console.log(nu);
+console.log(typeof nu);
+
+let undef = undefined ;
+console.log(undef);
+console.log(typeof undef);
+
+console.log(typeof Number);
+console.log(typeof String);
+console.log(typeof Boolean);
+console.log(typeof null);
+console.log(typeof undefined);
+console.log(typeof BigInt);
+console.log(typeof Symbol);
+console.log(typeof Object);
+console.log(typeof Array);
