c8ec4b2511be1022c96004fc59aab267d7b3c6c2
Shubham Malhan <shubhammalhan00@gmail.com>
2024-07-13 17:29:58 +0000

    more on day1

--- chaicode/day1.js
+++ chaicode/day1.js
@@ -1,43 +1,76 @@
-// Variables Declaration
+// // Variables Declaration
 
-var number = 123;
-console.log(number);
-console.log(typeof number);
+// var number = 123;
+// console.log(number);
+// console.log(typeof number);
 
-var string = "shubham";
-console.log(string);
-console.log(typeof string);
+// var string = "shubham";
+// console.log(string);
+// console.log(typeof string);
 
-const bool = false ;
-console.log(bool);
-console.log(typeof bool);
+// const bool = false ;
+// console.log(bool);
+// console.log(typeof bool);
 
-let obj = {
-    name: "shubham",
-    rollno: 123,
-    year: 2
+// let obj = {
+//     name: "shubham",
+//     rollno: 123,
+//     year: 2
+// };
+// console.log(obj);
+// console.log(typeof obj);
+
+// let arr = ["hero", "zero", "power"];
+// console.log(arr);
+// console.log(typeof arr);
+
+// let nu = null;
+// console.log(nu);
+// console.log(typeof nu);
+
+// let undef = undefined ;
+// console.log(undef);
+// console.log(typeof undef);
+
+// console.log(typeof Number);
+// console.log(typeof String);
+// console.log(typeof Boolean);
+// console.log(typeof null);
+// console.log(typeof undefined);
+// console.log(typeof BigInt);
+// console.log(typeof Symbol);
+// console.log(typeof Object);
+// console.log(typeof Array);
+
+// let reass = 0;
+// reass = 1;
+// console.log(reass);
+
+// const reassign = 1;
+// reassign = 2;
+// console.log(reassign);
+
+let change = 12;
+console.log(change);
+console.log(typeof change);
+
+change = "hello";
+console.log(change);
+console.log(typeof change);
+
+change = true;
+console.log(change);
+console.log(typeof change);
+
+change = {
+    name: "sam",
+    roll: 12
 };
-console.log(obj);
-console.log(typeof obj);
+console.log(change);
+console.log(typeof change);
 
-let arr = ["hero", "zero", "power"];
-console.log(arr);
-console.log(typeof arr);
+change = [1, 2, 3, 4];
+console.log(change);
+console.log(typeof change);
 
-let nu = null;
-console.log(nu);
-console.log(typeof nu);
 
-let undef = undefined ;
-console.log(undef);
-console.log(typeof undef);
-
-console.log(typeof Number);
-console.log(typeof String);
-console.log(typeof Boolean);
-console.log(typeof null);
-console.log(typeof undefined);
-console.log(typeof BigInt);
-console.log(typeof Symbol);
-console.log(typeof Object);
-console.log(typeof Array);
