2c0341954d9fc7211825685a7082e4bb9470a0a4
Shubham Malhan <shubhammalhan00@gmail.com>
2024-07-14 08:09:50 +0000

    modify day2

--- chaicode/day2.js
+++ chaicode/day2.js
@@ -25,4 +25,38 @@ let num9 = 5;
 let num10 = 10;
 console.log(num10%num9);
 
-console.log(num9 += num10); //  it just add num9 to num10 and put the value again in num9 like num9 = num9 + num10.
+console.log(num9 += num10); //  it just add num9 to num10 and put the value again in num9 like num9 = num10 + num9.
+
+console.log(num9 -= num10); // its like num9 = num10 - num9.
+let check = num10 > num9
+console.log(check);
+
+let s = "123";
+let m = 123;
+console.log(s === m);
+// (==) double equal to check the value of the both operator is equal or not it is not so much strict 
+// but (===) triple equal is used to check the value of the both operator with there data type its strict..
+
+console.log(s == m || s != m);
+
+// (&&) is used to check the two condition in which both the condition are true or we can use them multiple time 
+// (||) is used to check the two condition in which one condition has to be true .
+// (!) is used to make a false conditon true and vice-versa.
+let value = 4 ;
+console.log( value > -1 ? "positive" : "negative");
+
+//cintax for ternary operator 
+// condition ? trueValue : falseValue.
+
+let Num_1 = 24;
+let Num_2 = 32;
+console.log(Num_1 + Num_2);
+console.log(Num_1 - Num_2);
+console.log(Num_1 * Num_2);
+console.log(Num_1 / Num_2);
+console.log(Num_1 % Num_2);
+
+console.log((Num_1 < Num_2 && num2 > num1) || (Num_1 > Num_2 && num1 > num2));
+
+
+
