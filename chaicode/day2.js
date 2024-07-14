3c057e94a2a7518275186ed516fccca2659ba288
Shubham Malhan <shubhammalhan00@gmail.com>
2024-07-14 07:19:26 +0000

    day2

---
+++ chaicode/day2.js
@@ -0,0 +1,28 @@
+// Sum
+let num1 = 23;
+let num2 = 46;
+console.log(num1+num2);
+
+// Subtract
+let num3 = 100;
+let num4 = 21;
+console.log(num3-num4);
+
+//multiply 
+let num5 = 5;
+let num6 = 7;
+let num = num5 * num6;
+console.log(num);
+
+// Division
+let num7 = 64;
+let num8 = 3;
+num = num7 / num8;
+console.log(num);
+
+// Remainder
+let num9 = 5;
+let num10 = 10;
+console.log(num10%num9);
+
+console.log(num9 += num10); //  it just add num9 to num10 and put the value again in num9 like num9 = num9 + num10.
