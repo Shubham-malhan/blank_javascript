f7b084f8702e9d1f0f661fba56d6afd631e7bbc6
Shubham Malhan <shubhammalhan00@gmail.com>
2024-07-15 14:52:53 +0000

    day 3

---
+++ chaicode/day3.js
@@ -0,0 +1,17 @@
+let num = 0;
+
+if (num > 0){
+    console.log("Number is positive :");
+}else if (num == 0){
+    console.log("Number is a zero :");
+}else {
+    console.log("number is negative :");
+}
+
+let age = 18;
+
+if (age >= 18){
+    console.log("You can vote :");
+}else {
+    console.log("You can't vote :");
+}
