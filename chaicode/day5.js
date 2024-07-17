9405e5aab4ad791ddd44090e31b3d91f4ccdb05e
Shubham Malhan <shubhammalhan00@gmail.com>
2024-07-17 18:30:19 +0000

    day5

---
+++ chaicode/day5.js
@@ -0,0 +1,15 @@
+// write a function to check if a number is even or odd and log the result to the console.
+
+function num(number){
+    return number%2 == 0 ? console.log("even") : console.log("Odd")
+}
+
+num(2);
+
+//Write a function to calculate the square of a number and return the result.
+
+function sqr(num){
+    return num*num ;
+}
+
+console.log(sqr(2));
