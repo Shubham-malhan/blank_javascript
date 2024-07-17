504b497682338f8bf699a149909359840c62455a
Shubham Malhan <shubhammalhan00@gmail.com>
2024-07-17 07:34:22 +0000

    learn about do while

--- 05_iteration/two.js
+++ 05_iteration/two.js
@@ -1,14 +1,23 @@
 
 let index = 0;
 while (index <= 10) {
-    console.log(`value of index is : ${index}`);
+    // console.log(`value of index is : ${index}`);
     index = index +2;
     
 }  
 
-let myArray = ["Flash", "Batman", "spiderman"]
-let arr = 0;
-while (arr < myArray.length) {
-    console.log(`${myArray[arr]} is at the index ${arr}`);
-    arr++;
+let myArray = ["Flash", "Batman", "spiderman"];
+index = 0;
+while (index < myArray.length) {
+    // console.log(`${myArray[index]} is at the index ${index}`);
+    index++;
 }
+ 
+index = 11;
+
+// It will execute onces in the cause of wrong condition also..
+
+do {
+    console.log(`score is ${index}`);
+} while (index <= 10);
+
