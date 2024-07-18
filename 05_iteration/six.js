ded4e3cf365f1a5378f44e03e69ca564b0af8d3e
Shubham Malhan <shubhammalhan00@gmail.com>
2024-07-18 14:39:39 +0000

    six

---
+++ 05_iteration/six.js
@@ -0,0 +1,12 @@
+// const coding = ["js", "ruby", "java", "python", "cpp"]
+
+
+// const values = coding.forEach( (item) => {
+//     return item;
+// });
+
+// console.log(values);
+
+const myNums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]  
+
+
