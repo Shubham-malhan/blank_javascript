450638c1427d7092d1c24177e870d95080384571
Shubham Malhan <shubhammalhan00@gmail.com>
2024-07-19 15:51:36 +0000

    work on day6

---
+++ chaicode/day6.js
@@ -0,0 +1,19 @@
+let array = [1, 2, 3, 4, 5];
+
+console.log(array);
+
+console.log(array[0]);
+console.log(array[4]);
+
+array.push(1);
+console.log(array);
+
+array.pop();
+console.log(array);
+
+array.shift(1);
+console.log(array);
+
+array.unshift(1);
+console.log(array);
+
