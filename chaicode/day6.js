1c0c6eced03f1147eded87702d84fcce1dcb5a49
Shubham Malhan <shubhammalhan00@gmail.com>
2024-07-20 17:41:30 +0000

    done with day6

--- chaicode/day6.js
+++ chaicode/day6.js
@@ -1,19 +1,53 @@
 let array = [1, 2, 3, 4, 5];
 
-console.log(array);
+// console.log(array);
 
-console.log(array[0]);
-console.log(array[4]);
+// console.log(array[0]);
+// console.log(array[4]);
 
 array.push(1);
-console.log(array);
+// console.log(array);
 
 array.pop();
-console.log(array);
+// console.log(array);
 
-array.shift(1);
-console.log(array);
+array.shift();
+// console.log(array);
 
-array.unshift(1);
+array.unshift(9);
+// console.log(array);
+
+const map = new Map();
+map.set("1", 1);
+map.set("2", 4);
+map.set("3", 9);
+// map.set("3", 9);
+
+// console.log(map);
+
+const arr = array.filter( (num) => (num%2==0));
+
+// console.log(arr);
+
+// dont study reduce now 
+
+
+
+
+
 console.log(array);
+for (let i = 0; i < array.length; i++){
+    // console.log(array[i]);
+}
+
+array.forEach( (element) => console.log(element) )
+
+
+
+
+
+
+
+
+
 
