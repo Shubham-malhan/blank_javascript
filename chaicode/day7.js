a822d7d4e531888fa09047c3c361771aa9ca5f07
Shubham Malhan <shubhammalhan00@gmail.com>
2024-07-21 17:05:04 +0000

    day 7

--- chaicode/day7.js
+++ chaicode/day7.js
@@ -1,10 +1,28 @@
 const books = {
     tittle: "java",
     author: "shubham",
-    year: 2004
+    year: 2004,
 }
 
 // console.log(books);
 
-console.log(books.tittle);
-console.log(books.author);
+// console.log(books.tittle);
+// console.log(books.author);
+
+books.myfun = () => {
+    console.log(`${books.tittle} and ${books.author}`);
+}
+
+// console.log(books);
+// books.myfun();
+
+books.updateYear = (year) => {
+    books.year = year;
+    // console.log(this);
+}
+
+console.log(books);
+books.updateYear(2002);
+console.log(books);
+
+
