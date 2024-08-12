84b36f5e07db037dd4b22540cad9dc41e147b410
Shubham Malhan <shubhammalhan00@gmail.com>
2024-08-12 19:12:09 +0530

    more change

--- 01_basics/_comparsion.js
+++ 01_basics/_comparsion.js
@@ -4,6 +4,8 @@ console.log(null >= 0);
 
 
 
+
+
 // Because check operator (==) and comparsion operator (<,>,<=,>=) work differently
 // Comparisons convert null to a number, treating it as 0.
 // That's why (3) null >= 0 is true ans (1) null > 0 is false.
