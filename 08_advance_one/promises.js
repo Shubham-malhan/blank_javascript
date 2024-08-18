2b817d4cee8c268aad460dcd28b0de443694700a
Shubham Malhan <shubhammalhan00@gmail.com>
2024-08-18 23:45:36 +0530

    learn more about promise

--- 08_advance_one/promises.js
+++ 08_advance_one/promises.js
@@ -83,19 +83,19 @@ consumePromiseFive();
 //         const response = await fetch('https://jsonplaceholder.typicode.com/users')
 //     const data = await response.json();
 //     console.log(data);
-        
+
 //     } catch (error) {
 //         console.log("ERROR:", error);
-        
+
 //     }
 // }
 
 // getAllUsers();  
 
 fetch('https://jsonplaceholder.typicode.com/users')
-.then((response) =>{
+  .then((response) => {
     return response.json()
-})
-.then((data) => console.log(data))
-.catch((error) => console.log(error))
+  })
+  .then((data) => console.log(data))
+  .catch((error) => console.log(error))
 
